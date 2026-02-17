2948cb409b743e81e0dda3168c5055ed08a50dd2
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2026-02-17 11:48:28 +0100

    setuptools_scm versioning
    Derive package version from repository tags.

--- src/ppf/jabref/__init__.py
+++ src/ppf/jabref/__init__.py
@@ -3,10 +3,15 @@ ppf.jabref
 ++++++
 
 """
-# flake8: noqa
+try:
+    from importlib_metadata import version
+except ImportError:                                     # pragma: no cover
+    from importlib.metadata import version              # pragma: no cover
 
 # import every function, class, etc. that should be visible in the package
 from .jabref import *
+
+__version__ = version(__name__)
 
 del jabref
 del utils
